8438d388582e1157b0e9a580b6acd2143cdc3f66
zulema <azuul.rod98@gmail.com>
2024-03-11 18:54:30 -0300

    Pizza-JS

---
+++ index.js
@@ -0,0 +1,77 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+
+console.log(pizzas);
+
+
+
+let pizzas="nombre"
+console.log(pizzas.toUpperCase());
+
+/* Función que reciba un número por parametro e identifique en consola si es par o impar */
+function pizzas = [ 1, 2, 3, 4, 5, 6];
+const 
+
+
+/* Responder ¿hay alguna pizza que valga menos de $600 ? */
+console.log("Pizzas que valgan menos de $600 ");
+console.log();
+
+const PizzasBaratas=pizzas.every((pizza) => { return pizza < $600; });
+
+console.log(PizzasBaratas);
+
+/* El nombre de cada pizza con su respectivo precio */
+const nombre = pizzas.map(pizza => pizza.nombre + " " + pizza.precio);
+
+console.log(nombre);
+
+/* Todos los ingredientes de cada pizza */
